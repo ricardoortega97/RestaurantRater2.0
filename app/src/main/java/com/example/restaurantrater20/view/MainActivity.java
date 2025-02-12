734e982d678a8ce2d68666353c14d17cd631596a
ricardo ortega <ricardo.ortega97@outlook.com>
2025-02-12 00:17:30 -0500

    Main and Rating Activiy layouts implemented. rate button created.

---
+++ app/src/main/java/com/example/restaurantrater20/view/MainActivity.java
@@ -0,0 +1,40 @@
+package com.example.restaurantrater20.view;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.widget.Button;
+
+import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+
+import com.example.restaurantrater20.R;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        EdgeToEdge.enable(this);
+        setContentView(R.layout.activity_main);
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
+            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
+            return insets;
+        });
+
+        rateDish();
+    }
+
+    private void rateDish() {
+        Button btnRate = findViewById(R.id.buttonRate);
+        btnRate.setOnClickListener(v -> {
+            // Rate the dish
+
+            Intent intent = new Intent(MainActivity.this, RateDishActivity.class);
+            startActivity(intent);
+        });
+    }
+}
--- app/src/main/java/com/example/restaurantrater20/view/RateDishActivity.java
+++ app/src/main/java/com/example/restaurantrater20/view/RateDishActivity.java
@@ -1,6 +1,7 @@
-package com.example.restaurantrater20;
+package com.example.restaurantrater20.view;
 
 import android.os.Bundle;
+import android.widget.Button;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -8,17 +9,34 @@ import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
-public class MainActivity extends AppCompatActivity {
+import com.example.restaurantrater20.R;
+
+public class RateDishActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.activity_rate_dish);
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
             Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
+        });
+        cancelBtn();
+    }
+
+    private void rateDish() {
+        Button btnRate = findViewById(R.id.btnRate);
+        btnRate.setOnClickListener(v -> {
+            // Rate the dish
+        });
+    }
+
+    private void cancelBtn() {
+        Button btnCancel = findViewById(R.id.btnCancel);
+        btnCancel.setOnClickListener(v -> {
+            finish();
         });
     }
 }
